6fe96137ff5c33bf214c1111e953edcbafd2e81f
Sohaib Irfan <tihor.personal@gmail.com>
2026-05-09 12:39:44 +0530

    Made header dynamic

--- Distrributed File System/frontend/src/App.jsx
+++ Distrributed File System/frontend/src/App.jsx
@@ -18,7 +18,7 @@ function App() {
   return (
     <div className="min-h-screen bg-[#0f172a] text-white">
       <div className="max-w-7xl mx-auto p-6 space-y-6">
-        <Header />
+        <Header refresh={refresh} />
 
         <DashboardCards refresh={refresh} />
 
